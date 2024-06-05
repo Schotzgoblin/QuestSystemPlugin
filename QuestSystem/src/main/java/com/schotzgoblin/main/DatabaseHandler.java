cf0a2077bed0e411aa6019a12cda4995521a8787
Schotzgoblin <kammerem2@gmail.com>
2024-06-05 23:12:59 +0200

    Add Hibernate to the project

--- QuestSystem/src/main/java/com/schotzgoblin/main/DatabaseHandler.java
+++ QuestSystem/src/main/java/com/schotzgoblin/main/DatabaseHandler.java
@@ -1,4 +1,34 @@
 package com.schotzgoblin.main;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.*;
+import java.util.logging.Level;
+
 public class DatabaseHandler {
+    private final SessionFactory sessionFactory;
+    private final JavaPlugin plugin;
+
+    public DatabaseHandler(JavaPlugin plugin) {
+        this.plugin = plugin;
+        Configuration config = new Configuration();
+        config.configure(); // Configure using hibernate.cfg.xml
+        sessionFactory = config.buildSessionFactory();
+//        sessionFactory = new MetadataSources(config.buildSessionFactory()).buildMetadata().buildSessionFactory();
+    }
+
+    private Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    public void close() {
+        if (sessionFactory != null && !sessionFactory.isClosed()) {
+            sessionFactory.close();
+        }
+    }
 }
